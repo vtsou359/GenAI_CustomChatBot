0da017dc49e7cd5c13182b67a949eea3d0862f62
vtsou359 <vassilis@tsoubris.gr>
2025-03-05 19:31:01 +0200

    additional commits

---
+++ fncs/retrieval.py
@@ -0,0 +1,240 @@
+
+from typing import Any, List
+import numpy as np
+from scipy import spatial
+import pandas as pd
+
+
+def create_embeddings_batch(client,
+                            deployment_name: str,
+                            batch_size: int,
+                            df: pd.DataFrame,
+                            chunk_column: str,
+                            cost_per_thousand_tokens: float = 0.000125
+                            ) -> tuple[pd.DataFrame, Any]:
+    """
+    Creates embeddings for a given dataframe in batches by interacting with the client API. This function computes
+    embeddings for the text data in a specified column of the dataframe, adds the generated embeddings as a new
+    column, and calculates the total cost based on the number of tokens processed.
+
+    :param client: The client object used for interacting with the external embeddings API.
+    :param deployment_name: The name of the deployed embedding model to use for generating embeddings.
+    :param batch_size: The number of rows from the dataframe to process in each batch.
+    :param df: A pandas DataFrame containing the input data for which embeddings are to be generated.
+    :param chunk_column: The name of the column in the dataframe that contains the text data for embeddings.
+    :param cost_per_thousand_tokens: The cost per 1,000 tokens, used to calculate the total cost of processing.
+        Defaults to 0.000125 (price for: text-embedding-3-large) if not specified.
+    :return: A tuple containing the input dataframe with an additional "embeddings" column and the total cost
+        incurred for processing the embeddings.
+    :rtype: tuple(pd.DataFrame, float)
+    :raises ValueError: If the input dataframe is not a pandas DataFrame, the specified column is missing, or
+        if batch_size is less than 1.
+    :raises RuntimeError: If an error occurs during the embedding creation process.
+    """
+    # Input validation
+    if not isinstance(df, pd.DataFrame):
+        raise ValueError("Input df must be a pandas DataFrame")
+
+    if chunk_column not in df.columns:
+        raise ValueError(f"Column {chunk_column} not found in dataframe")
+
+    if batch_size < 1:
+        raise ValueError("batch_size must be positive")
+
+    # Initialize empty list for embeddings
+    embeddings = []
+    total_tokens = 0
+    try:
+        # Process in batches
+        for i in range(0, len(df), batch_size):
+            # Get batch of texts
+            batch_texts = df.iloc[i:i+batch_size][chunk_column].tolist()
+
+            # Generate embeddings for each row in the batch
+            for text in batch_texts:
+                emb_response = client.embeddings.create(
+                    model= deployment_name,
+                    input= text.replace("\n", " ")
+                )
+                # Extract and append the embedding
+                embedding = emb_response.data[0].embedding
+                embeddings.append(embedding)
+
+                # calculate total tokens
+                total_tokens += emb_response.usage.prompt_tokens
+
+        # Add embeddings to the dataframe
+        df["embeddings"] = embeddings
+
+        # Calculate total cost
+        cost = (total_tokens / 1_000) * cost_per_thousand_tokens
+
+        return df, cost
+
+    except Exception as e:
+        raise RuntimeError(f"Failed to create embeddings: {str(e)}")
+
+
+def get_embedding(text: str, client: Any, model="text-embedding-3-large", **kwargs) -> List[float]:
+    """
+    Computes the embedding vector for a given text using the specified client and model. The function
+    replaces newline characters in the input text to mitigate potential negative effects on
+    performance and then uses the provided client to generate the embedding.
+
+    :param text: The input text to be embedded.
+    :param client: The client instance used for generating the embedding.
+    :param model: The model identifier specifying the embedding model to be used. Defaults to
+        "text-embedding-3-large".
+    :param kwargs: Additional parameters to be passed to the client's embedding creation method.
+    :return: The embedding vector of the input text as a list of floats.
+    :rtype: List[float]
+    """
+    # replace newlines, which can negatively affect performance.
+    text = text.replace("\n", " ")
+    response = client.embeddings.create(input=[text], model=model, **kwargs)
+    return response.data[0].embedding
+
+
+def cosine_similarity(a, b):
+    """
+    Calculate the cosine similarity between two vectors. Cosine similarity measures the cosine
+    of the angle between two vectors in multi-dimensional space. The value returned is a scalar
+    between -1 and 1 where 1 indicates perfect similarity, 0 indicates orthogonality, and -1
+    indicates perfect dissimilarity.
+
+    :param a: First vector, typically represented as a numpy array.
+    :param b: Second vector, typically represented as a numpy array.
+    :return: Cosine similarity, a scalar value between -1 and 1.
+
+    """
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def cosine_distance(a, b):
+    """
+    Computes the cosine distance between two vectors.
+
+    The cosine distance is a measure of dissimilarity between two vectors,
+    calculated as 1 minus the cosine similarity. Cosine similarity measures
+    the cosine of the angle between two vectors in a multi-dimensional space.
+    This method is often used in machine learning and information retrieval
+    to determine the similarity between objects.
+
+    :param a: First vector, represented as a numpy array.
+    :param b: Second vector, represented as a numpy array.
+    :return: Cosine distance between input vectors, as a float.
+    """
+    return 1 - ( np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b)) )
+
+
+def distances_from_embeddings(
+    query_embedding: List[float],
+    embeddings: List[List[float]],
+    distance_metric="cosine",
+) -> List[List]:
+
+    """Return the distances between a query embedding and a list of embeddings."""
+
+    distance_metrics = {
+        "cosine": spatial.distance.cosine,
+        "L1": spatial.distance.cityblock,
+        "L2": spatial.distance.euclidean,
+        "Linf": spatial.distance.chebyshev,
+    }
+    distances = [
+        distance_metrics[distance_metric](query_embedding, embedding)
+        for embedding in embeddings
+    ]
+    return distances
+
+
+# search through the reviews for a specific product
+def search_reviews(df, embs_query, n=0, cosine = 'similarity'):
+
+    """
+    Searches through reviews in a DataFrame by calculating similarity or distance
+    between the query embedding and each review's embedding. Supports sorting
+    results by similarity or distance and optionally returns the top `n` matches.
+
+    :param df: DataFrame containing the reviews and their associated embeddings.
+               Should have a column named `embedding` that contains the vector
+               representations for each review.
+    :type df: pandas.DataFrame
+    :param embs_query: The query embedding to be compared against each review's
+                       embedding.
+    :type embs_query: numpy.ndarray
+    :param n: Number of top results to return. Defaults to 0, which means all
+              results are returned.
+    :type n: int, optional
+    :param cosine: Determines the metric to use for comparison. Accepts either
+                   'similarity' (for cosine similarity) or 'distance' (for
+                   cosine distance). Defaults to 'similarity'.
+    :type cosine: str, optional
+    :return: DataFrame with results sorted by similarity or distance, depending
+             on `cosine`. The number of results returned corresponds to `n`
+             (or all if `n` is 0).
+    :rtype: pandas.DataFrame
+    """
+    if cosine == 'similarity':
+        df["similarity"] = df.embeddings.apply(lambda x: cosine_similarity(x, embs_query))
+        if n==0:
+            results = df.sort_values("similarity", ascending=False)
+            return results
+        else:
+            results = df.sort_values("similarity", ascending=False).head(n)
+            return results
+
+    elif cosine == 'distance':
+        df["distance"] = df.embeddings.apply(lambda x: cosine_distance(x, embs_query))
+        if n==0:
+            results = df.sort_values("distance", ascending=True)
+            return results
+        else:
+            results = df.sort_values("distance", ascending=True).head(n)
+            return results
+    else:
+        raise ValueError("cosine param must be 'similarity' or 'distance'")
+
+
+# Function to add chunks to the context
+def control_chunk_context(chunks_sorted_df,
+                          current_token_count,
+                          max_token_count,
+                          tokenizer
+                          ):
+    """
+    Adds chunks of text to the context while respecting the maximum token count limit.
+
+    This function iterates through a DataFrame containing sorted text chunks, calculates
+    the token count for each text chunk, and appends them to a context list as long as
+    the total token count does not exceed the specified maximum. It stops adding chunks
+    once the token limit is reached.
+
+    :param chunks_sorted_df: The DataFrame containing text chunks sorted in a specific order.
+    :type chunks_sorted_df: pandas.DataFrame
+    :param current_token_count: The current token count before adding any text chunk.
+    :type current_token_count: int
+    :param max_token_count: The maximum allowable token count for the context.
+    :type max_token_count: int
+    :param context: A list that will contain the text chunks added within the token limit.
+    :type context: list
+    :param tokenizer: The tokenizer function used to encode text into tokens. Default is
+        the tokenizer for the "gpt-4o" model.
+    :type tokenizer: Callable
+    :return: A tuple containing the updated token count and the updated context list.
+    :rtype: tuple
+    """
+    # Initialize the context list
+    context = []
+    for text in chunks_sorted_df["text"].values:
+        # Calculate the token count for the current text
+        text_token_count = len(tokenizer.encode(text))
+        # Check if adding this text exceeds the token limit
+        if current_token_count + text_token_count <= max_token_count:
+            # If within limit, add text to context and update token count
+            context.append(text)
+            current_token_count += text_token_count
+        else:
+            # If token limit is exceeded, stop adding chunks
+            break
+
+    return context
---
+++ fncs/utilities.py
@@ -0,0 +1,168 @@
+"""Utility functions."""
+
+import json
+from typing import Any, Callable, Dict, List, Optional, Tuple
+
+import openai
+import pandas as pd
+from openai import AzureOpenAI, OpenAI
+
+
+def create_openai_client(
+        api_key: str,
+        organization: Optional[str] = None
+) -> OpenAI:
+    """
+    Creates and returns an instance of OpenAI client.
+
+    This function initializes a client object to interact with the OpenAI API service.
+    Authentication is done using an API key, which is required. Optionally, an organization ID
+    can be provided for users who belong to multiple organizations.
+
+    :param api_key: The API key for authentication with OpenAI services.
+    :param organization: Optional organization ID for users who belong to multiple organizations.
+        If not provided, the default organization associated with the API key will be used.
+    :return: An instance of OpenAI configured with the provided parameters.
+    :raises ValueError: If api_key is None or empty.
+    """
+    if not api_key:
+        raise ValueError("An API key must be provided.")
+
+    if organization:
+        return OpenAI(
+            api_key=api_key,
+            organization=organization
+        )
+    else:
+        return OpenAI(api_key=api_key)
+
+
+def create_azure_client(
+        api_version: str,
+        azure_endpoint: str,
+        ad_token: Optional[str] = None,
+        api_key: Optional[str] = None,
+        ad_token_provider: Optional[Callable[[], Optional[str]]] = None
+) -> AzureOpenAI:
+    """
+    Creates and returns an instance of AzureOpenAI client.
+
+    This function is intended to initialize
+    a client object to interact with the Azure OpenAI service. The client may be authenticated
+    using either an Azure AD token, a dynamic token provider, or an API key. If none of these
+    authentication methods is provided, an exception is raised. The `api_version` and
+    `azure_endpoint` parameters are required for configuring the client.
+
+    :param api_version: The API version of the Azure OpenAI service to be used.
+    :param azure_endpoint: The endpoint of the Azure OpenAI service.
+    :param ad_token: Azure AD token for authentication, provided as a string. Defaults to None.
+    :param api_key: API key for authentication, provided as a string. Defaults to None.
+    :param ad_token_provider: A callable function that provides an Azure AD token dynamically.
+        When called, it should return the token as a string or None. Defaults to None.
+    :return: An instance of AzureOpenAI configured with the provided parameters.
+    :raises ValueError: If neither `ad_token`, `api_key`, nor `ad_token_provider` is provided.
+    :raises ValueError: If more than one of `ad_token`, `api_key`, or `ad_token_provider` is provided.
+    """
+    if sum(bool(x) for x in [ad_token, api_key, ad_token_provider]) > 1:
+        raise ValueError("Provide only one of `api_key`, `ad_token`, or `ad_token_provider`, not multiple.")
+
+    if ad_token:
+        return AzureOpenAI(azure_ad_token=ad_token, api_version=api_version, azure_endpoint=azure_endpoint)
+    elif api_key:
+        return AzureOpenAI(api_key=api_key, api_version=api_version, azure_endpoint=azure_endpoint)
+    elif ad_token_provider:
+        token = ad_token_provider()
+        if not token:
+            raise ValueError("The token provider did not return a valid token.")
+        return AzureOpenAI(azure_ad_token=token, api_version=api_version, azure_endpoint=azure_endpoint)
+    else:
+        raise ValueError("Either `api_key`, `ad_token`, or `ad_token_provider` must be provided.")
+
+
+def response_generator(
+        client: Any,
+        chat_model: str,
+        prompts: List[Dict[str, Any]],
+        options: Optional[Dict[str, Any]] = None
+) -> Tuple[str, Any]:
+    """
+
+    Azure OpenAI Response Generator function with customizable options.
+
+    :param client: The Azure OpenAI/ OpenAI client instance.
+    :param chat_model: The specific chat model to use for generating responses.
+    :param prompts: The list of prompt messages to be sent to the chat model.
+    :param options: A dictionary of additional options for the OpenAI API call (e.g., temperature, max_tokens).
+                    Example: {"temperature": 0.2, "max_tokens": 500}
+    :return: A tuple containing the content of the first choice's message and the entire response object.
+
+    """
+    if options is None:
+        # Default options if no custom options are provided
+        options = {}
+
+    # Prepare parameters for the API call, combining the mandatory ones with additional options from the user
+    parameters = {"model": chat_model, "messages": prompts}
+    parameters.update(options)  # Unpack `options` dictionary and merge with default parameters
+
+    # Call Azure OpenAI chat completion endpoint with dynamic parameters
+    response = client.chat.completions.create(**parameters)
+
+    return response.choices[0].message.content, response
+
+
+def prompt_builder(
+        system_content: str,
+        user_content_prompt: str
+) -> List[Dict[str, str]]:
+    """
+
+    Prompt Builder function.
+
+    :param system_content: The content or message that defines the system's prompt in the conversation.
+    :param user_content_prompt: The content or message provided by the user in the conversation.
+    :return: A list of dictionaries, each containing the role ('system' or 'user') and their respective content.
+    """
+    return [{"role": "system", "content": system_content}, {"role": "user", "content": user_content_prompt}]
+
+
+def calculate_total_cost(
+        response_usage: Any,
+        deployment_name: str = "gpt-4o"
+) -> float:
+    """
+
+    Total Cost Calculation function.
+
+    :param response_usage: Data structure containing token usage details.
+    :param deployment_name: The model used for token calculation (default: 'GPT-4o').
+    :return: Total cost based on token usage and model type.
+
+    """
+
+    def calculate_cost_per_token(cost_per_million_tokens: float) -> float:
+        """
+        Convert cost per million tokens to cost per single token.
+
+        :param cost_per_million_tokens: Cost for 1,000,000 tokens.
+        :return: Cost per token.
+        """
+        return cost_per_million_tokens / 1_000_000
+
+    # Prices found in azure website:
+    if deployment_name == "gpt-4o-mini":
+        input_cost_per_million = 0.15835
+        output_cost_per_million = 0.6334
+
+    if deployment_name == "gpt-4o":
+        input_cost_per_million = 2.63916
+        output_cost_per_million = 10.5567
+
+    input_cost_per_token = calculate_cost_per_token(input_cost_per_million)
+    output_cost_per_token = calculate_cost_per_token(output_cost_per_million)
+
+    prompt_tokens = response_usage.prompt_tokens
+    completion_tokens = response_usage.completion_tokens
+
+    total_cost = (prompt_tokens * input_cost_per_token) + (completion_tokens * output_cost_per_token)
+    return total_cost
